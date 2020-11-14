9aa92dd9051ce44c13e388ee6f2b0f824afb7bf5
Dmytro Tkachuk <tkachukdimon@gmail.com>
2020-11-14 19:16:57 +0200

    addTask created

--- api/tasks/tasks.router.js
+++ api/tasks/tasks.router.js
@@ -1,5 +1,8 @@
 const { Router } = require('express');
+const tasksController = require('./tasks.controller')
 
 const router = Router();
+
+router.post('/addTask', tasksController.addTaskValidation, tasksController.addTask)
 
 module.exports = router;
