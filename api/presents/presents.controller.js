50f522dddd1354051001085071ed38e595dce540
Peter Shevchuk <raf02041994@gmail.com>
2020-11-16 21:04:18 +0200

    50% end-point add presents

---
+++ api/presents/presents.controller.js
@@ -0,0 +1,36 @@
+const PresentsModel = require('./presents.model');
+
+class PresentsController {
+  async addPresent(req, res, next) {
+    try {
+      let image;
+      const { title, childId, bal, file } = req.body;
+      await PresentsModel.create({
+        title,
+        childId,
+        bal,
+        image,
+        dateCreated: Date.now(),
+      });
+      return res.status(201).send('Present added');
+    } catch (err) {
+      next(err);
+    }
+  }
+  //   async removePresent(req, res, next) {
+  //     try {
+  //       const result = await PresentsModel.findByIdAndDelete(
+  //         req.params.presentId,
+  //       );
+  //       if (!result) return res.status(404).send({ message: 'Not found' });
+  //       return res.status(201).send('Present deleted');
+  //     } catch (err) {
+  //       next(err);
+  //     }
+  //   }
+  //   async buyPresent(req, res, next) {
+  //     // test
+  //   }
+}
+
+module.exports = new PresentsController();
--- api/presents/presents.router.js
+++ api/presents/presents.router.js
@@ -1,5 +1,10 @@
 const { Router } = require('express');
+const presentsController = require('./presents.controller');
 
 const router = Router();
+
+router.post('/addPresent', presentsController.addPresent);
+// router.post('/buyPresent', presentsController.buyPresent);
+// router.delete('/:presentId', presentsController.removePresent);
 
 module.exports = router;
