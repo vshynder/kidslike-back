07b96c6c370823bf5ab7bb1c6ad07793c30f9a69
OleynikDima <57680550+OleynikDima@users.noreply.github.com>
2020-11-16 23:01:00 +0200

    add google OAuth 2.0 v 0.1

--- api/token/token.controller.js
+++ api/token/token.controller.js
@@ -9,7 +9,7 @@ class TokenController {
       const authHeader = req.get('Authorization');
       let token;
 
-      token = !authHeader ? res.status(401,'Unauthorized').send({ message: 'Not authorized' }) : authHeader.split(' ')[1];
+      token = !authHeader ? res.status(401,'Unauthorized').send({ message: 'Not authorized ' }) : authHeader.split(' ')[1];
         let user;
       
         try {
@@ -17,10 +17,16 @@ class TokenController {
       } catch (error) {
         return res
           .status(401, 'Unauthorized')
-          .send({ message: 'Not authorized' });
+          .send({ message: 'Not authorized Sekret Token' });
       }
 
-      const new_token = await jwt.sign(
+      user = await authUserModel.findOne({ refresh_token: token });
+
+      if (!user) {
+        return res.status(401).send({ message: 'Not Found User' });
+      }
+
+      const access_token = await jwt.sign(
         {
           id: user._id,
           email: user.email,
@@ -31,9 +37,9 @@ class TokenController {
         },
       );
 
-      const new_refresh_token = await jwt.sign(
+      const refresh_token = await jwt.sign(
         {
-          id: user.id,
+          id: user._id,
           email: user.email,
         },
         process.env.SECRET_TOKEN,
@@ -44,13 +50,13 @@ class TokenController {
 
       await authUserModel.findByIdAndUpdate(
         user.id,
-        { token: new_token, refresh_token: new_refresh_token },
+        { access_token, refresh_token },
         { new: true },
       );
 
-      return res.status(201).send({
-        access_token: new_token,
-        refresh_token: new_refresh_token,
+      return res.status(200).json({
+        access_token,
+        refresh_token,
       });
     } catch (error) {
       console.log(error);
