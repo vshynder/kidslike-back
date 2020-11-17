dd713f2b5fd1a4cdffaa1871a76aa056ab34ac6e
velmor99 <volgagaz24100@gmail.com>
2020-11-17 02:08:36 +0200

    firs done registration

--- api/users/users.router.js
+++ api/users/users.router.js
@@ -1,5 +1,14 @@
 const { Router } = require('express');
+const AuthController = require('./users.controller');
 
 const router = Router();
+
+router.get('/verify/:verificationToken', AuthController.verificationEmail);
+
+router.post(
+  '/register',
+  AuthController.validateCreateUser,
+  AuthController.register,
+);
 
 module.exports = router;
