2b87b132304686c3070b2a2430f4324bfd346803
littwa <littwa777@gmail.com>
2020-11-18 22:15:21 +0200

    kl-10in

--- api/habbits/habbits.router.js
+++ api/habbits/habbits.router.js
@@ -2,9 +2,21 @@ const express = require('express');
 const habbitsRouter = express.Router();
 const habbitsController = require('./habbits.controller');
 
+habbitsRouter.post(
+  '/addhabbit',
+  habbitsController.validIdChild,
+  habbitsController.addHabbit,
+);
+
 habbitsRouter.get(
   '/getallhabbitsuser',
   habbitsController.getAllHabbitsChildrenByUser,
+);
+
+habbitsRouter.patch(
+  '/updatehabbit',
+  habbitsController.validIdHabbit,
+  habbitsController.updateHabbit,
 );
 
 module.exports = habbitsRouter;
