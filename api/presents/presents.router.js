07c3fa3379828ed910c73a9d0b8fc3474674cd0b
Andrii Makohin <andreas2010@ukr.net>
2020-11-26 02:41:03 +0200

    add images

--- api/presents/presents.router.js
+++ api/presents/presents.router.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const presentsController = require('./presents.controller');
+const {imageUploader} = require('./uploadImage')
 
 const router = Router();
 
 
-router.post('/addPresent', presentsController.addPresent, presentsController.validPresent);
+router.post('/addPresent', imageUploader, presentsController.addPresent, presentsController.validPresent);
 router.post('/buyPresent', presentsController.buyPresent);
 router.delete('/:presentId', presentsController.removePresent);
 router.get('/:userId',presentsController.getAllPresentsChild)
