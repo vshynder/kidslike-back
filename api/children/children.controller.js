db49a08d39d58f972077f358976c467ee8a5f15d
littwa <littwa777@gmail.com>
2020-11-27 19:48:33 +0200

    kl-08-fix+++

--- api/children/children.controller.js
+++ api/children/children.controller.js
@@ -5,10 +5,10 @@ const Joi = require('joi');
 class Controllers {
   addChild = async (req, res, next) => {
     try {
-      const isExisted = await ChildrenModel.findOne({ name: req.body.name });
-      if (isExisted) {
-        return res.status(409).send(`Child with this name exists`);
-      }
+      // const isExisted = await ChildrenModel.findOne({ name: req.body.name });
+      // if (isExisted) {
+      //   return res.status(409).send(`Child with this name exists`);
+      // }
 
       // req.user = { _id: '5fb313842e5c6c182c9b214f' }; //Заглушка, ожидает обьект req.user с полем _id Родителя
 
