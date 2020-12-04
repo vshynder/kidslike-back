cc12f60481072e267904b215cc3221a737a15bfb
Dmytro Tkachuk <tkachukdimon@gmail.com>
2020-12-04 02:48:00 +0200

    without gitignore

---
+++ config/index.js
@@ -0,0 +1,12 @@
+const Cloud = require('@google-cloud/storage');
+const path = require('path');
+
+const serviceKey = path.join(__dirname, './keys.json');
+
+const { Storage } = Cloud;
+const storage = new Storage({
+  keyFilename: serviceKey,
+  projectId: 'kidslikev2-295012',
+});
+
+module.exports = storage;
